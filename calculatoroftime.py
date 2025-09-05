c882b021e47096db0156426bec5f19e2da3c2735
Alan <119041020+Soul-Open@users.noreply.github.com>
2025-09-05 18:07:28 +0530

    Add files via upload

---
+++ calculatoroftime.py
@@ -0,0 +1,16 @@
+print("hello!")
+print("im gonna calculate the speed of time for you...")
+uni = 3.09*10*18*700
+mass = 1.5*10**53
+objmass = float(input("object mass (dont enter kg but suppose its in kg): "))
+state =  objmass / uni + 1
+print(f"\nThe state of time is: if its 0, time is stopped. if one or ONE<TWO, time is working. the state is: {state}")
+c = 299792458
+void = c
+equ = uni * mass * state + c / void
+actualt = equ / 3600
+print(f"The actual speed of time is: {actualt} billion seconds per seconds, 1s per s is 3600 seconds.\n The delay of time is 3600seconds but the speed is something else. \nHowever, the speed is affected due to time dilation. ")
+print(f"The time dilation states that the speed of an object will affect the speed of time. \nSuppose I send a kid at space at 99% {c} meters per seconds (or speed of light).\n And keep one on earth. The one that is space have time slower for him. So when he returns to earth at age of 25.\n The one on earth would be around hundred years old.")
+print("\nand 1seconds per seconds is the delay it can get upto. The speed is calculated by the formula of:")
+print(f"\nuniverse * massofuniverse * stateoftime * speed_of_light / void. or: {uni} * {mass} * {state} + {c} / {void}")
+print(f"\nVoid is also just 100% speed of light, All the units used are already used by nasa and other space agencies. Everything is CAREFULLY RESEARCHED.\n My facts may be wrong. But i just cant prove myself. Thats the work of NASA.\n\n Made by @AlanCodes0 on YT or Alan.")
